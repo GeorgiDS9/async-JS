b9c39c945de4c023c26fbab41a8f8b8736494cd5
“GeorgiDS9git config --global user.name GeorgiDS9git config --global user.email georgianachiriacweb@gmail.comgit config --global user.email georgianachiriacweb@github.com <georgianachiriacweb@gmail.com>
2021-04-01 19:24:29 -0400

    call functions with async/await

--- index.js
+++ index.js
@@ -5,27 +5,42 @@
 const loginUser = (email, password) => {
   return new Promise((resolve, reject) => {
     setTimeout (() => {
-      console.log('Processing email and passord to get token', email, password);
+      console.log('Processing email and passord to get token ', email, password);
       resolve({authtoken: 'sa84r8ufdscn'});
     }, 500); // show the token after 0.5 sec
   });
 };
 
-const getYouVideos = token => {
+const getYourVideos = token => {
   return new Promise((resolve, reject) => {
     setTimeout (() => {
-      console.log('Processing token' + token + 'to get videos');
+      console.log('Processing token ' + token + ' to get videos');
       resolve(['video1', 'video2', 'video3']);
     }, 1000); // show the list of videos after 1 sec
   });
 };
 
-const showVideoDetails = (video) => {
+const getVideoDetails = (video) => {
   return new Promise((resolve, reject) => {
     setTimeout (() => {
-      console.log('Processing email and passord to get token', email, password);
-      resolve({authtoken: 'sa84r8ufdscn'});
-    }, 2000); // show the details of the video after 2 sec
+      console.log('Processing video to get details ', video);
+      resolve({duration: '27 min', likes: '2040', dislikes: '83'});
+    }, 1500); // show the details of the video after 1.5 sec
   });
 };
 
+// Call the function (with promises)
+// loginUser("jane doe", "jane@janedoe.com")
+// .then(token => getYourVideos(token))
+// .then(videos => getVideoDetails(videos[1]))
+// .then(result => console.log(result))
+
+// Call the function (with async/await)
+// const alltogether = async () => {
+//   const token = await loginUser ("jane doe", "jane@janedoe.com")
+//   const video = await getYourVideos('abc123')
+//   const videoDetails = await getVideoDetails('videos[1]')
+//   console.log(videoDetails)
+// }
+
+// alltogether();
